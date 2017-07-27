ea74da2951d21f042da315198617ee078d96040f
“Vilde <vilde@vevatne.no>
2017-07-27 16:27:25 -0700

    make and import searchbar component

--- src/index.js
+++ src/index.js
@@ -4,15 +4,22 @@
 // have to explicitly import modules fo each individual file
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// must provide path for files me make ourselves, not for packages
+import SearchBar from './components/search_bar';
 // 1. Create component
 // 2. Take the component, put it on the page (in the DOM)
-
+const API_KEY = 'AIzaSyDOVi6sO3R6XWBD0CxjXUQO1sgZjhOHFIA';
 // const = declaring a variable that's immutable
 // a 'factory' that creates instances of components, a class
 // with arrow functions, the value of 'this' is different
 const App = () => {
 	// returns jsx = subset of js, looks like html
-	return <div>Hi!</div>;
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
 };
 
 // passing an instance of the class to the render function
