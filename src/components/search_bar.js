af42f00d6e049edc8112fe1e513a708d1fdab492
“Vilde <vilde@vevatne.no>
2017-07-28 09:57:51 -0700

    Add event handler for user input in searchbar component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,8 +8,17 @@ class SearchBar extends Component {
 	// must have render method
 	render() {
 		// must return jsx
-		return <input />;
+		// handle regular browser events by writing 'on#{nameOfEvent}={this.#{nameOfEventHandler}}'
+		// 'onChange' is a prop (property)
+		// define event handler right here instead
+		return <input onChange={event => console.log(event.taget.value)} />;
 	}
+	// event handler:
+	// have to pass it the event we would like to monitor
+	// onInputChange(event) {
+	// describes the context or information about the event that occurs
+	// console.log(event.target.value);
+	// }
 }
 
 export default SearchBar;
